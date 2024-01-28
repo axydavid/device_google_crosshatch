d9ad47da027363453b33b66e503d5bb200c5c41a
axydavid <axy_david@yahoo.com>
2024-01-28 03:04:30 +0100

    fix matrix error

--- BoardConfigLineage.mk
+++ BoardConfigLineage.mk
@@ -17,8 +17,8 @@ TARGET_KERNEL_SOURCE := kernel/google/msm-4.9
 TARGET_NEEDS_DTBOIMAGE := true
 
 # Manifests
-DEVICE_MANIFEST_FILE += device/google/crosshatch/lineage_manifest.xml
-DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += vendor/voltage/config/device_framework_matrix.xml
+#DEVICE_MANIFEST_FILE += device/google/crosshatch/lineage_manifest.xml
+#DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += vendor/voltage/config/device_framework_matrix.xml
 
 # Partitions
 AB_OTA_PARTITIONS += \
@@ -39,8 +39,8 @@ BOARD_SYSTEM_EXTIMAGE_PARTITION_RESERVED_SIZE := 92160000
 endif
 
 # SELinux
-BOARD_SEPOLICY_DIRS += device/google/crosshatch/sepolicy-lineage/dynamic
-BOARD_SEPOLICY_DIRS += device/google/crosshatch/sepolicy-lineage/vendor
+#BOARD_SEPOLICY_DIRS += device/google/crosshatch/sepolicy-lineage/dynamic
+#BOARD_SEPOLICY_DIRS += device/google/crosshatch/sepolicy-lineage/vendor
 
 # Verified Boot
 BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 3
